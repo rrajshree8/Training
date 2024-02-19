16eb8c99fa712e6e0f453632b928e8686061f9e5
rrajshree8 <89907125+rrajshree8@users.noreply.github.com>
2024-02-19 09:21:11 +0530

    Create Products.js

---
+++ React/14 Feb/Products.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Here are some of our amazing products:</p>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
