4c075f18d4aae9472923bf72c6d439635319f4df
rrajshree8 <89907125+rrajshree8@users.noreply.github.com>
2024-02-19 09:26:03 +0530

    Create slice.js

---
+++ React/15 Feb/src/store/slice.js
@@ -0,0 +1,26 @@
+// src/store/todoSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: []
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.todos.find(todo => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    }
+  }
+});
+
+export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
+export default todoSlice.reducer;
