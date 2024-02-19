10860d6a1d4a76d47b86958131dbf495c82b0495
rrajshree8 <89907125+rrajshree8@users.noreply.github.com>
2024-02-19 09:26:45 +0530

    Create App.js

---
+++ React/15 Feb/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Todo App</h1>
+        <TodoList />
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
